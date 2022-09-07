d222f5c34363279631df15bcb65440a5248d6815
ariramadh <ariramadhani24@gmail.com>
2022-09-07 15:32:29 +0700

    Add function

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const jsonToTable = require('json-to-table');
- 
+const jsonToTable = require('json-to-table'); 
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -22,10 +21,20 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('SELECT * FROM student', (err, result, fields) => {
-  if (err) throw err;
-//   console.log(result);
-  console.log(jsonToTable(result));
-})
+function open(){
+    var sql = "INSERT INTO student (id, fname, lname, id_number) VALUES ('6', 'Ari', 'Ramadhani', '2021')"
+    connection.query(sql, (err, result, fields) => {
+        if (err) throw err;
+            console.log("Insert success!!");
+      })    
+}
+
+function open(){
+    connection.query('SELECT * FROM student', (err, result, fields) => {
+        if (err) throw err;
+      //   console.log(result);
+        console.log(jsonToTable(result));
+      })
+}
 
 connection.end()
